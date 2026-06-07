38ac8058e69e87b6f821d4271fd2375be12a5890
José Tafur <dashpan806@gmail.com>
2026-06-07 08:41:45 -0500

    feat(mesas): optimizar el flujo de control y renderizado visual de mesas

--- src/main/java/com/web/restaurante/repository/MesaRepository.java
+++ src/main/java/com/web/restaurante/repository/MesaRepository.java
@@ -4,6 +4,9 @@ import com.web.restaurante.model.Mesa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MesaRepository extends JpaRepository<Mesa, Long> {
+    Optional<Mesa> findByNumero(Integer numero);
 }
